9851101e7c90c13c6776b396ccc2a2fc27921c16
Luke aka SwissalpS <161979+SwissalpS@users.noreply.github.com>
2025-09-17 14:38:55 +0200

    code-style suggestion
    adapted to other code in this repo and org.

--- noclip_hurt.lua
+++ noclip_hurt.lua
@@ -8,18 +8,20 @@ core.register_globalstep(function(dtime)
 
 	for _,player in pairs(core.get_connected_players()) do
 
-		local pos = player:get_pos() ; pos.y = pos.y + 0.5
-		local nod = core.get_node(pos)
-		local ndef = core.registered_nodes[nod.name]
+		local pos = player:get_pos()
+		pos.y = pos.y + 0.5
+		local node = core.get_node(pos)
+		local ndef = core.registered_nodes[node.name]
 
 		if ndef and ndef.walkable == true
-		and ndef.damage_per_second == 0
-		and ndef.groups.disable_suffocation ~= 1
-		and ndef.drawtype == "normal"
-		and not core.check_player_privs( player:get_player_name(), {noclip = true} ) then
+			and ndef.damage_per_second == 0
+			and ndef.groups.disable_suffocation ~= 1
+			and ndef.drawtype == "normal"
+			and not core.check_player_privs(player:get_player_name(), { noclip = true })
+		then
 
 			if player:get_hp() > 0 then
-				player:set_hp(player:get_hp() - 4, {suffocation = true})
+				player:set_hp(player:get_hp() - 4, { suffocation = true })
 			end
 		end
 	end
