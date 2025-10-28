61c939f0c76f30dc793ece3a45fcc5a6eae31f59
Niklp <nik@niklp.net>
2025-10-28 20:38:29 +0100

    Adjust noclip hurt detection (take both feet & head node into account)

--- noclip_hurt.lua
+++ noclip_hurt.lua
@@ -1,27 +1,30 @@
 local timer = 0
 
+local function is_solid_node(pos)
+	local ndef = core.registered_nodes[core.get_node(pos).name]
+	return ndef
+		and ndef.walkable == true
+		and ndef.groups.disable_suffocation ~= 1
+		and ndef.drawtype == "normal"
+end
+
 core.register_globalstep(function(dtime)
 
 	timer = timer + dtime
 	if timer < 1 then return end
 	timer = 0
 
-	for _, player in pairs(core.get_connected_players()) do
-
+	for _, player in ipairs(core.get_connected_players()) do
 		local pos = player:get_pos()
+
 		pos.y = pos.y + 0.5
-		local node = core.get_node(pos)
-		local ndef = core.registered_nodes[node.name]
+		if is_solid_node(pos) then
+			pos.y = pos.y + 1 -- Player's head
+			if is_solid_node(pos)
+				and player:get_hp() > 0
+				and not core.check_player_privs(player:get_player_name(), { noclip = true }) then
 
-		if ndef and ndef.walkable == true
-			and ndef.damage_per_second == 0
-			and ndef.groups.disable_suffocation ~= 1
-			and ndef.drawtype == "normal"
-			and not core.check_player_privs(player:get_player_name(), { noclip = true })
-		then
-
-			if player:get_hp() > 0 then
-				player:set_hp(player:get_hp() - 4, { suffocation = true })
+				player:set_hp(player:get_hp() - 1, { suffocation = true })
 			end
 		end
 	end
